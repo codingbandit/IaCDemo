2e556688162d1e452f672e5f75c3b289962de51d
codingbandit <carey.payette@gmail.com>
2024-09-24 19:52:18 -0400

    Storage account take 2

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg-techbash-iac" {
-  name     =  var.rg_name
+  name     = var.rg_name
   location = var.location
 }
 
 resource "azurerm_storage_account" "demo_storage_account" {
-  name                     = "demostorageaccount123" # Must be globally unique
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg-techbash-iac.name
   location                 = azurerm_resource_group.rg-techbash-iac.location
   account_tier             = "Standard"
