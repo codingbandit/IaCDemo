b7d42f1f2522f3b0fc88afe40da33f317917d920
codingbandit <carey.payette@gmail.com>
2024-09-25 16:59:12 -0400

    Demo time

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ resource "azurerm_resource_group" "rg-techbash-iac" {
   location = var.location
 }
 
-#resource "azurerm_storage_account" "demo_storage_account" {
-#  name                     = var.storage_account_name
-#  resource_group_name      = azurerm_resource_group.rg-techbash-iac.name
-#  location                 = azurerm_resource_group.rg-techbash-iac.location
-#  account_tier             = "Standard"
-#  account_replication_type = "LRS"
-#
-#  tags = {
-#    environment = "Demo"
-#  }
-#}
+resource "azurerm_storage_account" "demo_storage_account" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg-techbash-iac.name
+  location                 = azurerm_resource_group.rg-techbash-iac.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Demo"
+  }
+}
