a840eea839bc2a72e0194eefedae68103bfb943e
codingbandit <carey.payette@gmail.com>
2024-11-17 16:59:58 -0500

    change rg naming in TF

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "rg-techbash-iac" {
+resource "azurerm_resource_group" "rg-live360-iac" {
   name     = var.rg_name
   location = var.location
 }
 
 resource "azurerm_storage_account" "demo_storage_account" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg-techbash-iac.name
-  location                 = azurerm_resource_group.rg-techbash-iac.location
+  resource_group_name      = azurerm_resource_group.rg-live360-iac.name
+  location                 = azurerm_resource_group.rg-live360-iac.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
